134d0a6a6510a5cf2e14c02a2fddd23da178ce68
Sunny Xu <43919034+therealsunnyxu@users.noreply.github.com>
2025-08-10 18:33:29 -0700

    Site-wide utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import { Route, Routes } from 'react-router'
 import './App.css'
+import AccountView from './components/views/AccountView'
+import IndexView from './components/views/IndexView'
+import LoginView from './components/views/LoginView'
 
 function App() {
   return (
     <Routes>
-      <Route index element={<div />} />
-      <Route path="login" element={<div />} />
-      <Route path="logout" element={<div />} />
+      <Route index element={<IndexView />} />
+      <Route path="login" element={<LoginView />} />
       <Route path="reset-password">
         <Route index element={<div />} />
-        <Route path="confirm" element={<div />} />
+        <Route path=":uidb64/:token" element={<div />} />
+        <Route path="submitted" element={<div />} />
+        <Route path="error" element={<div />} />
         <Route path="done" element={<div />} />
       </Route>
       <Route path="dashboard">
         <Route index element={<div />} />
         <Route path="account">
-          <Route index element={<div />} />
+          <Route index element={<AccountView />} />
         </Route>
       </Route>
     </Routes>
---
+++ src/site.ts
@@ -0,0 +1,3 @@
+const SITE_URL = "http://localhost:8000";
+
+export default SITE_URL;
